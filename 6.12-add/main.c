6a03e013e1dd237b6033cc550aaab62907d752fd
qiushuang <1542867980@qq.com>
2018-12-06 16:50:28 +0800

    12

---
+++ 6.12-add/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a[1000];
+    int i;
+    int j=0;
+    for(i=0;i<1000;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]>0)
+            j+=a[i];
+        else
+            printf("%d",j);
+    }
+
+}
