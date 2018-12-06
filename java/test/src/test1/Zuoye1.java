e6af4bad5f8f5b845ce2f7a193a96a459fba448a
qiushuang <1542867980@qq.com>
2018-12-06 15:03:16 +0800

    123

--- java/test/src/test1/Zuoye1.java
+++
@@ -1,9 +0,0 @@
-package test1;
-
-public class Zuoye1 {
-	public static void main(String[] args){
-		for(int i=1;i<) {
-			
-		}
-	}
-}
--- java/test3/src/test3/eiei.java
+++
@@ -1,8 +0,0 @@
-package test3;
-
-public class eiei {
-	  public static void main(String[] args){
-		  dog eiei=new dog("eiei",2,'ÄÐ');
-		  System.out.println(eiei.getname());
-	  }
-}
